3ebd914d49d23e9ca7042183bc836039cfdc9e62
John MacFarlane <fiddlosopher@gmail.com>
2011-09-26 20:20:54 -0700

    Modified test.sh to use cabal build.

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+PROG=./dist/build/citeproc/citeproc
 for style in chicago-author-date.csl mhra.csl; do
 echo $style
 echo
-( cat | ./citeproc $style biblio.bib ) <<EOF
+( cat | $PROG $style biblio.bib ) <<EOF
 [[{"id":"item1","suffix":"test"}, {"id":"item2", "label":"chapter", "locator":"15", "suffix":["et ",["EMPH","passim"]]}],[{"id":"item3","author_in_text":true}]]
 EOF
 echo
